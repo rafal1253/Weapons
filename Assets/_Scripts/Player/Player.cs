aa41b1be065b96adc4e25960f2afa5ff910db420
rafal1253 <rafal1253@gmail.com>
2024-03-17 21:27:50 +0100

    Add using weapon handling

--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -9,12 +9,26 @@ public class Player : MonoBehaviour
     [SerializeField] Camera _camera;
     float _xRotation = 0f;
 
+    [Header("Equipment")]
+    [SerializeField] List<Weapon> _equippedWeapons;
+    private Weapon _activeWeapon;
 
     private Rigidbody _rb;
 
-    void Start()
+    void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        EquipStartingWeapon();
+    }
+
+    private void Update()
+    {
+        UseWeapon();
+        NextWeapon();
     }
 
     void FixedUpdate()
@@ -24,7 +38,7 @@ public class Player : MonoBehaviour
         RotateY();
     }
 
-
+    #region MotionControlHandling
     private void Move()
     {
         float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime;
@@ -50,4 +64,34 @@ public class Player : MonoBehaviour
 
         _camera.transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
     }
+    #endregion
+
+    #region WeaponHandling
+    private void EquipStartingWeapon()
+    {
+        if (_equippedWeapons.Count > 0)
+            _activeWeapon = _equippedWeapons[0]; 
+
+        // ADD equip weapon event
+    }
+
+    private void UseWeapon()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            _activeWeapon.Attack();
+    }
+
+    private void NextWeapon()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            int currentWeaponIndex = _equippedWeapons.IndexOf(_activeWeapon);
+            int newWeaponIndex = currentWeaponIndex + 1 == _equippedWeapons.Count ? 0 : currentWeaponIndex + 1;
+
+            _activeWeapon = _equippedWeapons[newWeaponIndex];
+        }
+    }
+
+    private void 
+    #endregion
 }
